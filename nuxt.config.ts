3e8dfc3bc3136896d079485efa108eab29d66611
guxuerui <im.guxuerui@gmail.com>
2023-09-07 11:22:17 +0800

    chore(nuxt.config.ts): add app head configuration for viewport, icons, and meta tags feat(nuxt.config.ts): add description meta tag with appDescription constant feat(public): add apple-touch-icon.png and nuxt.svg files

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import { appDescription } from './constants/index'
+
 export default defineNuxtConfig({
   devtools: { enabled: true },
   typescript: {
@@ -14,5 +16,20 @@ export default defineNuxtConfig({
   },
   colorMode: {
     preference: 'light',
+  },
+  app: {
+    head: {
+      viewport: 'width=device-width,initial-scale=1',
+      link: [
+        { rel: 'icon', href: '/favicon.ico', sizes: 'any' },
+        { rel: 'icon', type: 'image/svg+xml', href: '/nuxt.svg' },
+        { rel: 'apple-touch-icon', href: '/apple-touch-icon.png' },
+      ],
+      meta: [
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'description', content: appDescription },
+        { name: 'apple-mobile-web-app-status-bar-style', content: 'black-translucent' },
+      ],
+    },
   },
 })
