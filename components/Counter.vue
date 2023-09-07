7ccb0bf1b9743f32a4e6c68f952927121827b4bd
guxuerui <im.guxuerui@gmail.com>
2023-09-07 16:37:02 +0800

    feat(Counter.vue): add Counter component feat(count.ts): add useCount composable function for managing count state

---
+++ components/Counter.vue
@@ -0,0 +1,17 @@
+<script setup lang='ts'>
+const { count, inc, dec } = useCount()
+</script>
+
+<template>
+  <div class="inline-flex my-3">
+    <UButton color="orange" size="xl" class="rounded-full p-2" @click="dec()">
+      <div class="i-carbon-subtract" />
+    </UButton>
+    <div class="font-mono w-14 m-auto inline-block">
+      {{ count }}
+    </div>
+    <UButton color="pink" size="xl" class="rounded-full p-2" @click="inc()">
+      <div class="i-carbon-add" />
+    </UButton>
+  </div>
+</template>
