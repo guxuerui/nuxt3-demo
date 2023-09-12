3d4cbd2da6e8fc6378d73616d4af4edc192722f4
guxuerui <im.guxuerui@gmail.com>
2023-09-12 12:35:04 +0800

    fix(index.vue): change duration variable from 1500 to ref(5000) feat(index.vue): add carousel component with dynamic items and play/pause functionality

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-const duration = 1500
+const duration = ref(5000)
+
 const baseNumber = ref(0)
 
 const cubicBezierNumber = useTransition(baseNumber, {
@@ -10,11 +11,46 @@ const cubicBezierNumber = useTransition(baseNumber, {
 function toggle() {
   baseNumber.value = baseNumber.value === 100 ? 0 : 100
 }
+
+const items = ref<string[]>(['a', 'b', 'c', 'd'])
+const isPlay = ref(true)
+
+function togglePlay(val: boolean) {
+  isPlay.value = val
+}
 </script>
 
 <template>
   <main class="text-center">
-    <UButton @click="toggle">
+    <div
+      class="bg-teal-500 overflow-hidden h-[530px]"
+      @mouseenter="togglePlay(false)"
+      @mouseleave="togglePlay(true)"
+    >
+      <Caresoul
+        v-for="(item, index) in items"
+        :key="index"
+        :should-scroll="items.length > 3"
+        :duration="duration"
+        :is-play="isPlay"
+      >
+        <a
+          href="#"
+          class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
+        >
+          <img
+            class="object-fill w-full rounded-t-lg h-64 md:h-auto md:w-32 md:rounded-none md:rounded-l-lg ml-2"
+            src="https://cdn.vuetifyjs.com/images/cards/halcyon.png"
+            alt=""
+          >
+          <div class="flex flex-col justify-between p-4 leading-normal">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021-{{ item }}</h5>
+            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+          </div>
+        </a>
+      </Caresoul>
+    </div>
+    <UButton class="mt-4" @click="toggle">
       Transition
     </UButton>
 
